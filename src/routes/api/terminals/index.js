dff55137fc126e1b9f994b90de79b199872573f9
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-02 18:52:35 +0700

    feat: Created Get Terminal by ID API route.

--- src/routes/api/terminals/index.js
+++ src/routes/api/terminals/index.js
@@ -5,9 +5,19 @@ export default function () {
     router.route('/')
         .get(getAllTerminals)
 
+    router.route('/:id')
+        .get(getTerminalById)
+
     return router
 }
 
 const getAllTerminals = (req, res) => {
     res.send("All Terminals");
+}
+
+const getTerminalById = (req, res) => {
+    const { params } = req;
+    const { id } = params;
+
+    res.send(`Get terminal: ${id}`);
 }
