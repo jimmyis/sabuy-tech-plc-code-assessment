a7fe25db8f7b663d36092b6ef8427279f4c03efd
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-03 12:07:09 +0700

    feat: Updated API route set.
    - Added Base API route set.
    - Removed previous Base API routes. (Unused/Duplicated)

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,18 +1,12 @@
 import { Router } from "express";
-import terminalsApis from "./terminals/index.js";
+import baseApiRoutes from "./base/index.js";
+import terminalsApiRoutes from "./terminals/index.js";
 
 const router = Router();
 
-export default function ({ app }) {
-    
-    router.route('/')
-        .get(apiBase);
-
-    router.use('/terminals', terminalsApis())
+export default function (/* context */) {
+    router.use('/', baseApiRoutes());
+    router.use('/terminals', terminalsApiRoutes());
     
     return router
-}
-
-const apiBase = (req, res) => {
-    res.send("API route");
 }
