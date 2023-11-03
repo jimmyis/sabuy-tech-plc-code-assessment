a1280e673558154da3d6971c69d2ed156d773ddd
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-03 12:12:13 +0700

    feat(Terminal): Created Read Terminal by ID model.

--- src/models/terminals/index.js
+++ src/models/terminals/index.js
@@ -9,3 +9,16 @@ export const readAllTerminals = async ({ db }) => {
         throw (e);
     }
 }
+
+export const readTerminalById = async ({ db }, id) => {
+    try {
+        const result = await db
+            .query(`SELECT * from TERMINAL_INFO WHERE TERM_ID = ?`, 
+            [id]);
+
+        return serializeEscaper(result)
+    } catch (e) {
+        console.error(e);
+        throw (e)
+    }
+}
