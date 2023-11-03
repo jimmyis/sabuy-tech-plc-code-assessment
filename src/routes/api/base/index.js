e50401d6bd8d9801f702844c86bbb82488f4bfc1
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-03 12:03:55 +0700

    feat: Added Service Status checker route to Base API route set.

--- src/routes/api/base/index.js
+++ src/routes/api/base/index.js
@@ -5,6 +5,12 @@ const router = Router();
 
 
 export default function () {
+    router.route('/status')
+        .get(routeServiceStatus);
+
 
     return router
 }
+
+const routeServiceStatus = async (req, res) => res.send("OK");
+
