a433c7fae2bf9616370170278883df938b7a6a03
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-03 12:12:39 +0700

    feat(Terminal): Created Read Terminal by ID controller.

--- src/controllers/terminals/index.js
+++ src/controllers/terminals/index.js
@@ -1,7 +1,12 @@
 import {
     readAllTerminals,
+    readTerminalById,
 } from "../../models/terminals/index.js";
 
 export const getAllTerminals = async ({ db }) => {
     return await readAllTerminals({ db });
+}
+
+export const getTerminalById = async ({ db }, id) => {
+    return await readTerminalById({ db }, id);
 }
