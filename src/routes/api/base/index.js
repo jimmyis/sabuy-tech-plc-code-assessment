7c2187352e08705c96cde2909730e05cd1cc4f31
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-03 12:05:11 +0700

    feat: Added Update Test Table route to Base API route set.

--- src/routes/api/base/index.js
+++ src/routes/api/base/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { updateTestTable } from "../../../controllers/base-api/index.js"
 
 
 const router = Router();
@@ -8,6 +9,8 @@ export default function () {
     router.route('/status')
         .get(routeServiceStatus);
 
+    router.route('/update')
+        .put(routeUpdateTestTable);
 
     router.route('*')
         .all((req, res) => res.status(404).send("Not Found"));
@@ -17,3 +20,23 @@ export default function () {
 
 const routeServiceStatus = async (req, res) => res.send("OK");
 
+const routeUpdateTestTable = async (req, res) => {
+    try {
+        const result = await updateTestTable(req.context);
+
+        if (result.error) {
+            switch (result.code) {
+                case 'LIMIT_EXCEEDED':
+                    return res.status(410).send("ERROR: Update Limit Exceeded");
+                default:
+                    return res.status(500).send("ERROR");
+            }
+        }
+
+        res.send("The MIN field update successfully.");
+
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("ERROR");
+    }
+}
