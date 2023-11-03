036125bf46a855a0f9c23dd1eab94965c00e1417
Jimmyis <jimmy.israkhan@gmail.com>
2023-11-03 12:14:20 +0700

    feat(Terminal): Created Edit Terminal controller.

--- src/controllers/terminals/index.js
+++ src/controllers/terminals/index.js
@@ -2,6 +2,7 @@ import {
     readAllTerminals,
     readTerminalById,
     insertTerminal,
+    updateTerminalById,
 } from "../../models/terminals/index.js";
 
 export const getAllTerminals = async ({ db }) => {
@@ -14,4 +15,8 @@ export const getTerminalById = async ({ db }, id) => {
 
 export const addTerminal = async ({ db }, data) => {
     return await insertTerminal({ db }, data);
+}
+
+export const editTerminal = async ({ db }, data) => {
+    return await updateTerminalById({ db }, data);
 }
